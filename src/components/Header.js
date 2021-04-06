cddaddf044c75a24c6895f0cfd81b7534356241d
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-04-06 14:55:01 +0700

    Basic completion

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button
+        text={showAdd ? 'Close' : 'Add'}
+        color={showAdd ? 'red' : 'green'}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracer',
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+export default Header;
---
+++ src/components/Task.js
@@ -0,0 +1,24 @@
+import { FaTimes } from 'react-icons/fa';
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => {
+        onToggle(task.id);
+      }}
+    >
+      <h3 key={task.id}>
+        {task.text}{' '}
+        <FaTimes
+          style={{ color: 'red', pointer: 'cursor' }}
+          onClick={() => {
+            onDelete(task.id);
+          }}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
